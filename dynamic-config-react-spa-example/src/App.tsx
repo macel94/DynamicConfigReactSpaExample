5164674274c22e2884c604804a9bcaf0eb83c557
francesco.belacca <francesco.belacca@agic.cloud>
2023-04-30 13:04:04 +0200

    better showcased when the config really gets loaded

--- dynamic-config-react-spa-example/src/App.tsx
+++ dynamic-config-react-spa-example/src/App.tsx
@@ -9,15 +9,19 @@ import { useConfig } from './configuration/useConfig';
 const App: React.FC = () => {
   const [configLoadingState, setConfigLoadingState] = useState<"loading" | "ready" | "error">("loading");
   const { config, setConfig } = useConfig();
+  const [ configWasFetched, setConfigWasFetched ] = useState<boolean>(false);
 
   useEffect(() => {
     fetch(dynamicConfigUrl)
       .then(response => response.json())
       .then((deserializedJsonConfig) => {
         setTimeout(() => {
+          setConfigWasFetched(true);
           setConfig(deserializedJsonConfig);
           console.log("Global config fetched: ", deserializedJsonConfig);
-          setConfigLoadingState("ready");  
+          setTimeout(() => {
+            setConfigLoadingState("ready");
+          }, 2000);
         }, 5000);
       })
       .catch(e => {
@@ -30,7 +34,7 @@ const App: React.FC = () => {
     return (
       <>
         <h1>Global config is loading...</h1>
-        <h2>Current apiUrl value: {config.apiUrl}</h2>
+        <h2 style={{ color: (configWasFetched ? "green" : "red"), textAlign: "center" }}>Current apiUrl value: {config.apiUrl}</h2>
         <p>the app is delaying the load of the config file to show that even if global context we can manually load and use the dynamic config...</p>
       </>);
   }
